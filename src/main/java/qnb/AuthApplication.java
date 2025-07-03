464f7ebce77df1773e60ead6db1f6040cd5184f1
dahyeon1216 <sally1023p@gmail.com>
2025-07-03 22:38:09 +0900

    AuthApplication.java 수정

--- src/main/java/qnb/AuthApplication.java
+++ src/main/java/qnb/AuthApplication.java
@@ -1,9 +1,14 @@
 package qnb;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+        }
+)
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
