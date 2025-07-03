4411e383b0bcbcecb4ccbbbd63ab9863f5897bb5
dahyeon1216 <sally1023p@gmail.com>
2025-07-03 11:09:52 +0900

    chore: securityconfig 수정

--- src/main/java/qnb/common/config/SecurityConfig.java
+++ src/main/java/qnb/common/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         //공개 경로
