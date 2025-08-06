97675e5744f58c9a9f1f624c29e57463b51c0755
dahyeon1216 <sally1023p@gmail.com>
2025-08-06 13:00:59 +0900

    feat: 프로필 URL이 NULL 일 때 기본 이미지 추가

--- src/main/java/qnb/user/dto/UserInfoResponseDto.java
+++ src/main/java/qnb/user/dto/UserInfoResponseDto.java
@@ -18,19 +18,28 @@ public class UserInfoResponseDto {
     private LocalDate birthDate;
     private Gender gender;
     private String phoneNumber;
-    //readingTaste가 null 또는 빈 문자열이면 false, 그렇지 않으면 true로 반환되게 구현
     private boolean hasReadingTaste;
+
+    // 디폴트 프로필 이미지 S3 URL
+    private static final String DEFAULT_PROFILE_URL =
+            "https://qnb-profile-images.s3.ap-southeast-2.amazonaws.com/default/profile.jpeg";
 
     public UserInfoResponseDto(User user) {
         this.userId = user.getUserId();
         this.userEmail = user.getUserEmail();
         this.userNickname = user.getUserNickname();
-        this.profileUrl = user.getProfileUrl();
+
+        // profile_url이 null이면 디폴트 이미지 사용
+        this.profileUrl = (user.getProfileUrl() != null)
+                ? user.getProfileUrl()
+                : DEFAULT_PROFILE_URL;
+
         this.name = user.getName();
         this.birthDate = user.getBirthDate();
         this.gender = user.getGender();
         this.phoneNumber = user.getPhoneNumber();
-        this.hasReadingTaste = user.getHasReadingTaste();
 
+        //readingTaste가 null 또는 빈 문자열이면 false, 그렇지 않으면 true로 반환되게 구현
+        this.hasReadingTaste = user.getHasReadingTaste();
     }
 }
