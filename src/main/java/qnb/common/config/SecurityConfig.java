2e31e1f9d4446d5e98726f0d7a5d9a3c718c8df0
dahyeon1216 <sally1023p@gmail.com>
2025-08-21 17:06:42 +0900

    api: 주소 수정

--- src/main/java/qnb/common/config/SecurityConfig.java
+++ src/main/java/qnb/common/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 //개발용
                 "http://localhost:3000",
-                "http://54.252.246.118:3000",
+                "http://3.107.68.205:3000",
 
                 //아이폰
                 "http://192.168.0.109:3000",
